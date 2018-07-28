cece065b6515a9b212b54d37e01d1877ccff8a79
Obed <obejuela4@gmail.com>
2018-07-27 21:30:03 -0500

    Login normal

--- server/server.js
+++ server/server.js
@@ -14,10 +14,8 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-
-app.use(require('./routes/usuario'));
-
-
+//Importamos el archivo que tiene todas nuestras rutas
+app.use(require('./routes/index'));
 
 mongoose.connect(process.env.URLDB, (err, res) => {
 
